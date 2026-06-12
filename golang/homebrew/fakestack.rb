fa52784762b86b98614800770a0af0f2301375fd
Devendra Pratap Singh <dps.manit@gmail.com>
2026-06-12 10:59:30 +0530

    chore: update Homebrew formula to v2.0.0 with SHA256 hashes

--- golang/homebrew/fakestack.rb
+++ golang/homebrew/fakestack.rb
@@ -1,26 +1,26 @@
 class Fakestack < Formula
   desc "High-performance database generator with realistic fake data"
   homepage "https://github.com/0xdps/fake-stack"
-  version "1.0.1"
+  version "2.0.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/0xdps/fake-stack/releases/download/v#{version}/fakestack-darwin-arm64"
-      sha256 "" # darwin-arm64
+      sha256 "7746a42485235308c669ca468b6224686dd513fee394e72530e8f7d999357a50"
     else
       url "https://github.com/0xdps/fake-stack/releases/download/v#{version}/fakestack-darwin-amd64"
-      sha256 "" # darwin-amd64
+      sha256 "2daa3716b73a7f8e9393cb780d819178987037d0149da59b0ac45db9d0b57fc4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/0xdps/fake-stack/releases/download/v#{version}/fakestack-linux-arm64"
-      sha256 "" # linux-arm64
+      sha256 "52e5576a25f1d9de496383693e889e4ad866dd3c5ffc8b80c4ec41bc0792b3f9"
     else
       url "https://github.com/0xdps/fake-stack/releases/download/v#{version}/fakestack-linux-amd64"
-      sha256 "" # linux-amd64
+      sha256 "d046eece8eb50fc799c1ad59788ca469c49e0620f87e66224b75b81a510a9077"
     end
   end
 
